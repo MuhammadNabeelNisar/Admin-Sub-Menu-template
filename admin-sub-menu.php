c3b4f4250a9ae86f1a4bab28496577e90fe1ab54
nabeel <muhammad.nabeel.nisar@gmail.com>
2022-01-28 22:46:35 +0500

    initial commit

--- admin-sub-menu.php
+++ admin-sub-menu.php
@@ -1,19 +1,12 @@
 <?php
 
 /**
- * Plugin Name:       our word filter plugin
+ * Plugin Name:       Admin Sub Menu
  * Plugin URI:        https://example.com/plugins/the-basics/
  * Description:       Handle the basics with this plugin.
  * Version:           1.0.0
- * Requires at least: 5.2
- * Requires PHP:      7.2
  * Author:            Muhammad Nabeel Nisar
- * Author URI:        https://author.example.com/
- * License:           GPL v2 or later
- * License URI:       https://www.gnu.org/licenses/gpl-2.0.html
- * Update URI:        https://example.com/my-plugin/
- * Text Domain:       my-basics-plugin
- * Domain Path:       /languages
+ * Author URI:        https://github.com/MuhammadNabeelNisar
  */
 
  if ( ! defined( 'ABSPATH' ) ) exit; // Exit if accessed directly
